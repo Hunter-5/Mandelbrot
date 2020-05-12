b340f4e1c2a396995a3dea22acafb132ac7b7be7
William <hren_agent33@icloud.com>
2020-05-12 11:05:04 -0700

    Added window using Xlib

---
+++ cplane.hpp
@@ -0,0 +1,15 @@
+/*#ifndef MANDELBROT_H
+#define MANDELBROT_H
+
+class Plane{
+    private:
+        Display *dis;
+        int screen;
+        Window win;
+        GC gc;
+    public:
+        Plane();
+};
+
+#endif // MANDELBROT_H
+*/
